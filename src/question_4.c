604815fd5327494527aba029472ff8c19b54606a
Your Name <you@example.com>
2022-11-04 01:19:32 +0330

    question_4.c: Add newline end of printf

--- src/question_4.c
+++ src/question_4.c
@@ -12,6 +12,6 @@
 
 int main()
 {
-    printf("The answer to the question of Life, the Universe, and Everything is 42.");
+    printf("The answer to the question of Life, the Universe, and Everything is 42.\n");
     return 0;
 }
