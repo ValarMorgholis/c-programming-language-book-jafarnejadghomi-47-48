6075373d62e0e67c9a0eebf1da91c1f501583ba6
Your Name <you@example.com>
2022-11-04 01:13:31 +0330

    Add header comment to all files

--- src/question_12.c
+++ src/question_12.c
@@ -1,3 +1,13 @@
+/**
+ * @file question_12.c
+ * @author M.M.Mirzaie (mohamadmahdimirzaieii@gmail.com)
+ * @version 0.1
+ * @date 2022-11-04
+ *
+ * @copyright Copyright (c) 2022
+ *
+ */
+
 #include <stdio.h>
 
 #define YEAR_PER_SECOND (double)31560000
--- src/question_6.c
+++ src/question_6.c
@@ -1,3 +1,13 @@
+/**
+ * @file question_6.c
+ * @author M.M.Mirzaie (mohamadmahdimirzaieii@gmail.com)
+ * @version 0.1
+ * @date 2022-11-04
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+
 #include <stdio.h>
 
 int main()
