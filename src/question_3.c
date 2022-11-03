6ae7554dfa9d8544b1e181242d67bce96bc99f7a
Your Name <you@example.com>
2022-11-04 01:07:09 +0330

    Add header comment to question_3

--- src/question_3.c
+++ src/question_3.c
@@ -1,3 +1,14 @@
+/**
+ * @file question_3.c
+ * @author M.M.Mirzaie (mohamadmahdimirzaieii@gmail.com)
+ * @brief In the line 14 after main() there is a semicolon that due a compiler error
+ * @version 0.1
+ * @date 2022-11-04
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+
 #include <stdio.h>
 
 int main()
